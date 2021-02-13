2947eeacc46185097583dfb1064d0c7969aca36b
Adrian Jankowski <jankowski.adrian13@gmail.com>
2021-02-13 21:26:39 +0100

    Refactor regarding sonar-lint.

--- src/main/java/api/dictionary/ApiRequestsParams.java
+++ src/main/java/api/dictionary/ApiRequestsParams.java
@@ -2,10 +2,13 @@ package api.dictionary;
 
 public class ApiRequestsParams {
 
-    public final static String REGRES_IN_API_BASE_URI = "https://reqres.in";
-    public final static String REGRES_IN_API_BASE_PATH = "/api";
+    public static final String REGRES_IN_API_BASE_URI = "https://reqres.in";
+    public static final String REGRES_IN_API_BASE_PATH = "/api";
+    public static final String REGRES_IN_API_LIST_USERS_SERVICE = "/users";
+    public static final String REGRES_IN_API_USER_SERVICE = "/users/";
+    public static final String REGRES_IN_API_UNKNOWN_SERVICE = "/unknown/";
 
-    public final static String REGRES_IN_API_LIST_USERS_SERVICE = "/users";
-    public final static String REGRES_IN_API_USER_SERVICE = "/users/";
-    public final static String REGRES_IN_API_UNKNOWN_SERVICE = "/unknown/";
+    private ApiRequestsParams() {
+        throw new UnsupportedOperationException();
+    }
 }
--- src/main/java/api/pojo/SupportPojo.java
+++ src/main/java/api/pojo/SupportPojo.java
@@ -1,5 +1,10 @@
 package api.pojo;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SupportPojo {
 
     private String url;
