15fea693262de1f2d24f6a9832da249d3f673a7f
Adrian Jankowski <jankowski.adrian13@gmail.com>
2021-02-14 22:08:29 +0100

    Moved all api requests to specific class.

--- src/main/java/api/request/RegresInApiRequest.java
+++ src/main/java/api/request/RegresInApiRequest.java
@@ -2,11 +2,14 @@ package api.request;
 
 import api.dictionary.ApiRequestsParams;
 import io.restassured.response.Response;
+import org.json.JSONObject;
 
 import static io.restassured.RestAssured.given;
 
 
 public class RegresInApiRequest {
+
+    // *** GET ***
 
     public Response getListUsers(String param, Object value) {
         return given()
@@ -15,5 +18,31 @@ public class RegresInApiRequest {
                 .queryParam(param, value)
                 .when()
                 .get(ApiRequestsParams.REGRES_IN_API_LIST_USERS_SERVICE);
+    }
+
+    public Response getUnknownService(int id) {
+        return given()
+                .baseUri(ApiRequestsParams.REGRES_IN_API_BASE_URI)
+                .basePath(ApiRequestsParams.REGRES_IN_API_BASE_PATH)
+                .when()
+                .get(ApiRequestsParams.REGRES_IN_API_UNKNOWN_SERVICE + id);
+    }
+
+    public Response getUser(int userId) {
+        return given()
+                .baseUri(ApiRequestsParams.REGRES_IN_API_BASE_URI)
+                .basePath(ApiRequestsParams.REGRES_IN_API_BASE_PATH)
+                .get(ApiRequestsParams.REGRES_IN_API_USER_SERVICE + userId);
+    }
+
+    // *** POST ***
+
+    public Response postNewUser(JSONObject requestParams) {
+        return given()
+                .baseUri(ApiRequestsParams.REGRES_IN_API_BASE_URI)
+                .basePath(ApiRequestsParams.REGRES_IN_API_BASE_PATH)
+                .body(requestParams)
+                .when()
+                .post(ApiRequestsParams.REGRES_IN_API_USER_SERVICE);
     }
 }
--- src/test/java/automation_practice/ApiTests.java
+++ src/test/java/automation_practice/ApiTests.java
@@ -1,6 +1,5 @@
 package automation_practice;
 
-import api.dictionary.ApiRequestsParams;
 import api.pojo.CreatedUserDataPojo;
 import api.pojo.UserListPojo;
 import api.pojo.UserPojo;
@@ -10,8 +9,6 @@ import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
-
-import static io.restassured.RestAssured.given;
 
 @Listeners(TestListener.class)
 public class ApiTests {
@@ -58,30 +55,23 @@ public class ApiTests {
         requestParams.put("name", name);
         requestParams.put("job", job);
 
-        given()
-                .baseUri(ApiRequestsParams.REGRES_IN_API_BASE_URI)
-                .basePath(ApiRequestsParams.REGRES_IN_API_BASE_PATH)
-                .body(requestParams)
-                .when()
-                .post(ApiRequestsParams.REGRES_IN_API_USER_SERVICE)
+        CreatedUserDataPojo createdUserDataPojo = regresInApiRequest
+                .postNewUser(requestParams)
                 .as(CreatedUserDataPojo.class);
+
+        Assert.assertEquals(name, createdUserDataPojo.getName());
     }
 
     private void checkUnknown(int id, int expectedStatusCode) {
-        given()
-                .baseUri(ApiRequestsParams.REGRES_IN_API_BASE_URI)
-                .basePath(ApiRequestsParams.REGRES_IN_API_BASE_PATH)
-                .when()
-                .get(ApiRequestsParams.REGRES_IN_API_UNKNOWN_SERVICE + id)
+        regresInApiRequest
+                .getUnknownService(id)
                 .then()
                 .statusCode(expectedStatusCode);
     }
 
     private void checkUser(int userId, String expectedName) {
-        UserPojo user = given()
-                .baseUri(ApiRequestsParams.REGRES_IN_API_BASE_URI)
-                .basePath(ApiRequestsParams.REGRES_IN_API_BASE_PATH)
-                .get(ApiRequestsParams.REGRES_IN_API_USER_SERVICE + userId)
+        UserPojo user = regresInApiRequest
+                .getUser(userId)
                 .as(UserPojo.class);
 
         Assert.assertEquals(user.getData().getFirst_name(), expectedName);
